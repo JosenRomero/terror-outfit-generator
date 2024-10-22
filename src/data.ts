d55d79bfce897b2309696fc62356fa503879b48f
Jose <romerojosemanuel@hotmail.com>
2024-10-21 19:15:14 -0600

    add a new example showcase

--- src/data.ts
+++ src/data.ts
@@ -24,5 +24,11 @@ export const examples: ImageItem[] = [
     modified: "https://res.cloudinary.com/josenromero/image/upload/e_extract:prompt_face/e_gen_background_replace:prompt_a%20darkness%20witch/e_grayscale/f_auto/q_auto/v1/terror-makeup-generator-images/wcbciurw3ia0ftt7hgps?_a=BBGE6FZE0",
     option: "witch",
     prompt: ""
+  },
+  {
+    original: "https://res.cloudinary.com/josenromero/image/upload/f_auto/q_auto/v1/terror-makeup-generator-images/quvz3drrahzg3prqenmt?_a=BBGABlZE0",
+    modified: "https://res.cloudinary.com/josenromero/image/upload/e_extract:prompt_face/e_gen_background_replace:prompt_a%20wonderwoman/f_auto/q_auto/v1/terror-makeup-generator-images/quvz3drrahzg3prqenmt?_a=BBGABlZE0",
+    option: "custom option",
+    prompt: "a wonderwoman"
   }
 ]
